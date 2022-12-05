d59dd4c49af7f336c13c67f43a6c50fa8d4199a9
Elaine Zhao <99935579+ElaineZhao92@users.noreply.github.com>
2022-12-05 14:45:13 +0800

    #pr part, update router

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -7,24 +7,24 @@ const {
   GetDashboard,
   DeleteRepo,
   //new interface about design
-  GetDesignMessage,
-  getTopic,
-  getTopicFrequency,
-  getDesignFrequency
 } = require("../controllers/dash");
 const { CheckUser, CreateUser } = require("../controllers/user");
-// =========== add ===============
+// =========== company ===============
 const {
-  RepoGetCommitters,
-  RepoGetIssues,
-  RepoGetStargazers,
   getCommitterData,
   getIssueData,
   getStargazerData,
 } = require("../controllers/company");
 // ===============================
 const { GetFrequency } = require("../controllers/frequency");
+// ========== pr ==============
+const { 
+  getDesignFrequency ,
+  getTopic,
+  getTopicFrequency,
+} = require("../controllers/design");
 
+// ============================
 
 router.route("/import").post(GetMessage);
 router.route("/login").post(CheckUser);
@@ -32,20 +32,16 @@ router.route("/register").post(CreateUser);
 router.route("/search").post(SearchRepoName);
 router.route("/dashboard").post(GetDashboard);
 router.route("/delete").post(DeleteRepo);
-// =========== add design information saver =============
-router.route("/importDesign").post(GetDesignMessage);
-// =========== add ===============
+// =========== add design =============
+router.route("/getDesign").post(getDesignFrequency);
+router.route("/getTopic").post(getTopic);
+router.route("/getTopicFrequency").post(getTopicFrequency);
+// =========== add company=============
 router.route("/committerCompany").post(getCommitterData);
 router.route("/stargazerCompany").post(getStargazerData);
 router.route("/issueCompany").post(getIssueData);
-// ===============================
-// ===============================
-// get frequecny
+// ===========add frequency ======
 router.route("/getFrequency").post(GetFrequency);
 // ===============================
-// ============= getters about design ===============
-router.route("/getTopic").post(getTopic),
-router.route("/getTopicFrequency").post(getTopicFrequency),
-router.route("/getDesignFrequency").post(getDesignFrequency)
 
 module.exports = router;
